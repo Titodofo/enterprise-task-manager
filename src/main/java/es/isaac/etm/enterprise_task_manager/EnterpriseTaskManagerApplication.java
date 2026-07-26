d5be1753ae574a578c3e4c1a3c3419fe1a9527e7
bt0432 <isaac.lopez@alumnos.upm.es>
2026-07-26 20:25:59 +0100

    Spring 0

---
+++ src/main/java/es/isaac/etm/enterprise_task_manager/EnterpriseTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package es.isaac.etm.enterprise_task_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/isaac/etm/enterprise_task_manager/EnterpriseTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.isaac.etm.enterprise_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
