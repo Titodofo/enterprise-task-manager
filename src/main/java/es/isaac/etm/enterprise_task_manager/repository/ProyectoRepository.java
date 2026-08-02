bb8d2a6014ef8cd403cdb56916df03649ecfcdb2
bt0432 <isaac.lopez@alumnos.upm.es>
2026-08-02 16:04:17 +0100

    Ticket 23

--- src/main/java/es/isaac/etm/enterprise_task_manager/repository/ProyectoRepository.java
+++ src/main/java/es/isaac/etm/enterprise_task_manager/repository/ProyectoRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProyectoRepository extends JpaRepository<Proyecto, Integer> {
-
-}
+public interface ProyectoRepository extends JpaRepository<Proyecto, Integer> {}
